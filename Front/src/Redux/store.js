2d851c4c63d1632b2bdb888ac226130c656147d1
jeremingolla <jeremingolla03@gmail.com>
2023-10-12 17:54:21 -0300

    Reducer

--- Front/src/Redux/store.js
+++ Front/src/Redux/store.js
@@ -1,7 +1,7 @@
 import {createStore, applyMiddleware} from "redux";
 
 import thunk from "redux-thunk";
-import reducer from "./Reducer";
+import reducer from "./reducer.js";
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
