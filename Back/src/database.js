d8fc4727d9bf189b8f6183837deed90ad36d2d8a
jeremingolla <jeremingolla03@gmail.com>
2023-10-12 12:13:28 -0300

    Cambios

--- Back/src/database.js
+++ Back/src/database.js
@@ -1,14 +1,17 @@
 
 const { Sequelize } = require('sequelize');
 const Iphone = require("./models/Iphone")
+require('dotenv').config()
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
+  DB_BASE
 } = process.env;
-const sequelize = new Sequelize('techno', 'postgres', 'belgrano2012', {
-  host: 'localhost',
-  dialect: 'postgres',
+const sequelize = new Sequelize(DB_BASE, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: 'postgres', 
   logging: false
 });
+
 Iphone(sequelize)
 
 module.exports = {
