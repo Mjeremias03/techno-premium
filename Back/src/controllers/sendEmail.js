7a9e8d8cd2cb57e2bda58bc80b4ed727d4eaa846
jeremingolla <jeremingolla03@gmail.com>
2023-10-20 15:18:47 -0300

    Nuevo

--- Back/src/controllers/sendEmail.js
+++ Back/src/controllers/sendEmail.js
@@ -1,4 +1,4 @@
-const transporte = require("./Trans");
+const transporte = require("./trans");
 
 const sendEmail = async (req, res) => {
   try {
@@ -11,19 +11,20 @@ const sendEmail = async (req, res) => {
         html: `
         <div style="background: linear-gradient(to bottom, #E6D0FF, #B897FF); text-align: center; padding: 20px; display: flex; flex-direction: column; color: #fff;">
         <div style="flex: 1;">
-          <p style="font-size: 24px; margin-bottom: 20px;">¡Bienvenido, ${name}!</p>
-          <p style="font-size: 18px;">Te damos la bienvenida a nuestra plataforma.</p>
-          <p style="font-size: 18px;">Estaremos en contacto para informarte sobre nuevas ofertas, adquisiciones, accesorios.</p>
-          <p style="font-size: 18px; margin: 20px 0;">Si necesitas algo, no dudes en consultarnos. Estamos aquí para ayudarte con cualquier pregunta que tengas, ya sea sobre stock, servicios o cualquier otro tema.</p>
-          <a href="https://api.whatsapp.com/send?phone=3518005218&text=Hola!%20Quiero%20consultar%20stock%20y%20formas%20de%20pago" target="_blank">
-            <button style="background-color: #00ff00; color: #fff; font-size: 18px; padding: 10px 20px; border: none; cursor: pointer; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); transition: box-shadow 0.3s;">
-              ¡Contáctanos en WhatsApp!
-            </button>
-          </a>
-        </div>
-        <div style="flex: 1; text-align: center;">
-          <img src="https://techno-premium.vercel.app/nuevo.jpg" alt="Welcome Image" style="max-width: 100%; margin: 20px 0;">
-        </div>
+        <p style="font-size: 24px; margin-bottom: 20px; color: #fff;">¡Bienvenido, ${name}!</p>
+        <p style="font-size: 18px; color: #fff;">Te damos la bienvenida a nuestra plataforma.</p>
+        <p style="font-size: 18px; color: #fff;">Estaremos en contacto para informarte sobre nuevas ofertas, adquisiciones, accesorios.</p>
+        <p style="font-size: 18px; margin: 20px 0; color: #fff;">Si necesitas algo, no dudes en consultarnos. Estamos aquí para ayudarte con cualquier pregunta que tengas, ya sea sobre stock, servicios o cualquier otro tema.</p>
+        <a href="https://api.whatsapp.com/send?phone=3518005218&text=Hola!%20Quiero%20consultar%20stock%20y%20formas%20de%20pago" target="_blank">
+          <button style="background-color: #00ff00; color: #fff; font-size: 18px; padding: 10px 20px; border: none; cursor: pointer; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); transition: box-shadow 0.3s;">
+            ¡Contáctanos en WhatsApp!
+          </button>
+        </a>
+      </div>
+      <div style="flex: 1; text-align: center;">
+        <img src="https://techno-premium.vercel.app/nuevo.jpg" alt="Welcome Image" style="max-width: 100%; margin: 20px 0;">
+      </div>
+      
       </div>      
         `,
       };
